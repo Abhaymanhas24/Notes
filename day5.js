10f2bcbed6896a75bb2fcaf537bad9ed0f5bb701
Abhaymanhas24 <abhay.manhas@proclink.com>
2024-06-04 09:41:19 +0530

    new

---
+++ day5.js
@@ -0,0 +1,24 @@
+/**
+//  * Multiplies two numbers.
+//  * @param {number} x - The first number.
+//  * @param {number} y - The second number.
+//  * @returns {number} The product of x and y.
+//  * @example
+//  * multiply(2, 3); // returns 6
+//  */
+// function multiply(x, y) {
+//     return x * y;
+//   }
+
+// Write a function to make it uppercase
+
+/**
+ *
+ * @param {string} text
+ * @returns {string}
+ */
+function upperCase(text) {
+  return text.toUpperCase();
+}
+var text = "hello world";
+console.log(upperCase(text));
